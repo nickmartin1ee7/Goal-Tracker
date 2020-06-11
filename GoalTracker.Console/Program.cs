69f0e65a25378b506677e877fffe9d11e5593b82
Nick Martin <nmmart04@louisville.edu>
2020-06-10 20:06:52 -0400

    Added console because forms is hard

---
+++ GoalTracker.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace GoalTracker.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
